ed276af4f82bad79db0462c66d56784481b61bbf
Jeremie <jeremie@movify.be>
2018-12-09 14:25:01 +0100

    Created initial Xcode project

---
+++ SweetLeapYears/SweetLeapYears/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SweetLeapYears
+//
+//  Created by Jérémie Vincke on 09/12/2018.
+//  Copyright © 2018 Jérémie Vincke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
