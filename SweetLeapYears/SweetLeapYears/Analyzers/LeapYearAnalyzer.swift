f50b7d410a6ef92d168ce3ec307f23595bf2ff6b
Jeremie <jeremie@movify.be>
2018-12-09 14:27:25 +0100

    Implemented LeapYearAnalyzer.isLeapYear method & added unit tests

--- SweetLeapYears/SweetLeapYears/Analyzers/LeapYearAnalyzer.swift
+++ SweetLeapYears/SweetLeapYears/Analyzers/LeapYearAnalyzer.swift
@@ -8,13 +8,54 @@
 
 import Foundation
 
+/*
+ ==============================================================
+ Documentation (source : http://codingdojo.org/kata/LeapYears/)
+ ==============================================================
+ 
+ Acceptance Criteria:
+ 
+ C1. All years divisible by 400 ARE leap years (so, for example, 2000 was indeed a leap year)
+ 
+ C2. All years not divisible by 4 are NOT leap years (e.g. 2017, 2018, 2019)
+ C3. All years divisible by 100 but not by 400 are NOT leap years (so, for example, 1700, 1800, and 1900 were NOT leap years, NOR will 2100 be a leap year)
+ 
+ C4. All years divisible by 4 but not by 100 ARE leap years (e.g., 2008, 2012, 2016)
+ 
+ 
+ */
+
 //MARK: - Leap year analyzer
 
 internal struct LeapYearAnalyzer {
     
     /// Returns true if the given year is a leap year, false otherwise.
     static func isLeapYear(_ year: Int) -> Bool {
-        fatalError() // TODO modify this
+        
+        // C1
+        guard year % 400 != 0 else {
+            return true
+        }
+        
+        // C2
+        guard year % 4 == 0 else {
+            return false
+        }
+        
+        // C3
+        guard year % 100 != 0 || year % 400 == 0 else {
+            return false
+        }
+        
+        // We have a potential candidate ; let's ensure it's indeed a leap year
+        
+        // C4 : we already know this year is divisible by 4 thanks to C2. Yet, year can still be divisible by 100, and therefore not be a leap year
+        guard year % 100 != 0 else {
+            return false
+        }
+        
+        return true // This year is a leap year
+        
     }
     
 }
